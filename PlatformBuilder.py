478e6056121570ce0258774285c5fa0cbdd6b3df
samdmarshall <me@samdmarshall.com>
2014-11-12 17:11:36 -0500

    changing platform builder code

--- PlatformBuilder.py
+++ PlatformBuilder.py
@@ -8,12 +8,9 @@ import plistlib
 from subprocess import CalledProcessError
 
 # Globals
-kSDKSettingsPlistName = 'SDKSettings.plist';
-kSDKSettingsPlistPath = '';
-kSDKSettingsPlist = '';
 kVerboseLogLevel = 0;
-kPrivateSDK = '';
-kBaseSDKPath = '';
+kInstallPlatform = '';
+kPlatformsPath = '';
 # Helper Functions
 def v_log(message, level, kVerboseLogLevel):
     if kVerboseLogLevel >= level:
@@ -54,8 +51,8 @@ def resolve_platform_path():
 # Main
 def main(argv):
     parser = argparse.ArgumentParser();
-    parser.add_argument('-p','--platform', help='path to platform template to build', required=True, action='store');
-    parser.add_argument('-u','--update', help='update platform from template', action='store_true');
+    parser.add_argument('-p','--platform', help='path to platform folder to install', required=True, action='store');
+    parser.add_argument('-u','--update', help='update platform folder', action='store_true');
     parser.add_argument('-f','--force', help='force action', action='store_true');
     parser.add_argument('-v','--verbose', help='add verbosity', action='count');
     args = parser.parse_args();
@@ -65,7 +62,19 @@ def main(argv):
     else:
         kVerboseLogLevel = args.verbose;
     
+    platform_name = os.path.basename(args.platform);
+    kPlatformsPath = resolve_platform_path();
+    platform_exists = platform_name in os.listdir(kPlatformsPath);
+    kInstallPlatform = os.path.join(kPlatformsPath, platform_name);
     
+    if platform_exists == True and args.force == True:
+        v_log('Removing previous install of \''+platform_name+'\'. This will take a few minutes',0,kVerboseLogLevel);
+        shutil.rmtree(kInstallPlatform);
+        platform_exists = False;
+    
+    if platform_exists == False:
+        v_log('Installing Platform...',0, kVerboseLogLevel);
+        shutil.copytree(args.platform, kInstallPlatform);
 
 
 if __name__ == "__main__":
